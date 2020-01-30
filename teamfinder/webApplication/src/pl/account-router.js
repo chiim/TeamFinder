ac50e74d38f22bd37dd2f4669af1b75c7863483a
Axel-Axelsson <Axel92_x@hotmail.com>
2020-01-30 14:04:44 +0100

    account hbs files html code added

--- teamfinder/webApplication/src/pl/account-router.js
+++ teamfinder/webApplication/src/pl/account-router.js
@@ -4,15 +4,23 @@ const expressHandlebars = require('express-handlebars')
 const router = express.Router()
 
 router.get('/login', function (request, response) {
-  response.render("login.hbs")
+  response.render("account-login.hbs")
 })
 
 router.post('/login', function (request, response) {
   response.redirect("/")
 })
 
-router.get('/register', function (request, response) {
-  response.render("register.hbs")
+router.get('/sign-up', function (request, response) {
+  response.render("account-sign-up.hbs")
+})
+
+router.get('/profile', function (request, response) {
+  response.render("account-profile.hbs")
+})
+
+router.get('/edit', function(request, response){
+  response.render("account-edit.hbs")
 })
 
 module.exports = router
